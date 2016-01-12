8a9dbb736db68d559fc9512bb0f3004b761217bf
Joseph Kriefall <joe.kriefall@ilmservice.com>
2016-01-12 09:43:50 -0600

    Reducing reducer boilerplate

---
+++ src/TheEight.WebApp/client/app/common/ActionBuilder.js
@@ -0,0 +1,36 @@
+﻿import { createAction, handleActions } from "redux-actions";
+import { transform } from "lodash";
+
+export default class {
+    constructor () {
+        this.registeredReducers = {};
+        this.defaultState = {};
+    }
+
+    setDefaultState(state) {
+        this.defaultState = state;
+    }
+
+    registerReducer(key, reducer, payloadCreator = null) {
+        this.registeredReducers[key] = { reducer, payloadCreator };
+    }
+
+    getActionCreators() {
+        const actions = Object.create(this.registeredReducers);
+
+        transform(actions, (accum, val, key) => {
+            accum[key] = createAction(key, val.payloadCreator);
+        });
+
+        return actions;
+    }
+
+    getReducer() {        
+        const reducers = Object.create(this.registeredReducers);
+
+        transform(reducers, (accum, val, key) => {
+            accum[key] = val.reducer;
+        });
+        return handleActions(reducers, this.defaultState);
+    }
+};
