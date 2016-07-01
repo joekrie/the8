d2d0edb71dc154b0b680e1c70877afc414142924
Joe Kriefall <joe.kriefall@gmail.com>
2016-07-01 03:46:46 -0500

    Couldn't make key unique across tables

--- src/TheEight.Database/Views/ERG_EVENT_RESULT_CALCULATIONS.sql
+++ src/TheEight.Database/Views/ERG_EVENT_RESULT_CALCULATIONS.sql
@@ -6,16 +6,16 @@ WITH SCHEMABINDING AS
 		res.[AttendeeId],
 		CASE
 			WHEN pc.[DistanceInMeters] IS NOT NULL THEN pc.[DistanceInMeters]
-			ELSE [dbo].[FN__Calculate_Distance_In_Meters](res.SplitInMilliseconds, pc.DurationInSeconds * 1000)
+			ELSE [dbo].[FN__Calculate_Distance_In_Meters](res.[SplitInMilliseconds], pc.[DurationInSeconds] * 1000)
 		END AS [DistanceInMeters],
 		CASE
 			WHEN pc.[DurationInSeconds] IS NOT NULL THEN pc.[DurationInSeconds]
-			ELSE [dbo].FN__Calculate_Duration_In_Milliseconds(res.SplitInMilliseconds, pc.DistanceInMeters) / 1000
+			ELSE [dbo].[FN__Calculate_Duration_In_Milliseconds](res.[SplitInMilliseconds], pc.[DistanceInMeters]) / 1000
 		END AS [DurationInSeconds],
 		res.[SplitInMilliseconds]
 	FROM [dbo].[ERG_EVENT_RESULTS] AS res
 	INNER JOIN [dbo].[ERG_EVENT_PIECES] AS pc
-		ON pc.EventId = res.EventId AND pc.PieceOrder = res.PieceOrder
+		ON pc.[EventId] = res.[EventId] AND pc.[PieceOrder] = res.[PieceOrder]
 
 GO
 
--- src/TheEight.Database/Views/TEAM_MEMBERS_IN_TEAM_ROLE_FLAGS.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE VIEW [dbo].[TEAM_MEMBERS_IN_TEAM_ROLES_FLAGS]
-WITH SCHEMABINDING AS 
-	SELECT
-		[TeamMemberId],
-		SUM([TeamRoleId]) AS TeamRoleFlag
-	FROM [dbo].[TEAM_MEMBERS_IN_TEAM_ROLES] AS tmr
-	GROUP BY [TeamMemberId]
