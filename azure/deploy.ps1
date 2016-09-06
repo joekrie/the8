2d116316e1a13516164b701857336c7259bc9433
Joe Kriefall <joe.kriefall@gmail.com>
2016-09-05 22:50:51 -0500

    Moved Azure Resource Group to azure folder

--- azure/deploy.ps1
+++ azure/deploy.ps1
@@ -4,3 +4,7 @@
 $SecureAzureKey = ConvertTo-SecureString $env:AZURE_KEY -AsPlainText -Force
 $AzureCreds = New-Object System.Management.Automation.PSCredential ("86ca6e54-2a1f-4d4c-84e6-fbe68e659e82", $SecureAzureKey)
 Login-AzureRmAccount -Credential $AzureCreds -ServicePrincipal -TenantId "81dd0ff5-473f-42c0-b5b6-1515970a60b4" -SubscriptionId "777c3a6c-ac90-4291-8628-3df75fe12521"
+
+$ResourceGroup = If ($env:APPVEYOR_REPO_BRANCH -eq "master") {"the8-prod"} Else {"the8-dev"}
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName "the8-sandbox" -TemplateFile "azuredeploy-test.json"
